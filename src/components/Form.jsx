9e3062aace67be945ab0be119e3a43cd3d4afdec
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-06-28 06:40:16 -0300

    Requisito 01: Finalizado

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -12,7 +12,7 @@ class Form extends React.Component {
         <Input labelName="atributo 2" type="number" testId="attr2-input" />
         <Input labelName="atributo 3" type="number" testId="attr3-input" />
         <Input labelName="imagem" type="text" testId="image-input" />
-        <Select options="normal;raro;muito raro" />
+        <Select options="normal;raro;muito raro" testId="rare-input" />
         <Input labelName="super Trunfo" type="checkbox" testId="trunfo-input" />
         <input value="Salvar" type="button" data-testid="save-button" />
       </div>
--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -6,33 +6,33 @@ class Select extends React.Component {
     return prop.split(';');
   }
 
-  convertSpace(str) {
-    console.log(str);
-    if (str.includes(' ')) {
-      console.log('hi');
-      const words = str.split(' ');
-      const wordFormated = this.firstToUpperCase(words[1]);
-      console.log(wordFormated);
-      str = `${words[0]}${wordFormated}`;
-    }
-    return str;
-  }
+  // convertSpace(str) {
+  //   console.log(str);
+  //   if (str.includes(' ')) {
+  //     console.log('hi');
+  //     const words = str.split(' ');
+  //     const wordFormated = this.firstToUpperCase(words[1]);
+  //     console.log(wordFormated);
+  //     str = `${words[0]}${wordFormated}`;
+  //   }
+  //   return str;
+  // }
 
-  firstToUpperCase(str) {
-    const letter = str[0];
-    const restWord = str.slice(1);
-    return letter.toUpperCase() + restWord;
-  }
+  // firstToUpperCase(str) {
+  //   const letter = str[0];
+  //   const restWord = str.slice(1);
+  //   return letter.toUpperCase() + restWord;
+  // }
 
   render() {
-    const { options } = this.props;
+    const { options, testId } = this.props;
     const convertedOptions = this.convertProp(options);
     // console.log(convertedOptions);
     const optionsTags = convertedOptions.map(
       (op, i) => (
         <option
           key={ i }
-          value={ this.convertSpace(op) }
+          value={ op }
         >
           { op }
         </option>
@@ -41,8 +41,7 @@ class Select extends React.Component {
     console.log(optionsTags);
     return (
       <label htmlFor="rarity">
-        <select>
-          <option value="">Escolha a raridade</option>
+        <select data-testid={ testId }>
           { optionsTags }
         </select>
       </label>
@@ -52,6 +51,7 @@ class Select extends React.Component {
 
 Select.propTypes = {
   options: PropTypes.string.isRequired,
+  testId: PropTypes.string.isRequired,
 };
 
 export default Select;
